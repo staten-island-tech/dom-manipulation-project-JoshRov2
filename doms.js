da1292a1094d49c95bb33b91a9bd2dd8d2624b63
JoshRov2 <joshr16@nycstudents.net>
2023-10-16 09:24:48 -0400

    update

---
+++ doms.js
@@ -0,0 +1,22 @@
+const DOMselectors = {
+    form: document.queryselector("#form"),
+    firstName: document.querySelector(".first-name"),
+
+
+
+
+    
+    //grab all the text input
+    //grab all the h2s
+//document.querySelectorAll("h2"),
+    }
+    
+   // DOMselectors.form.addEventListener("submit", function(event){
+    //event.preventDefault();
+   // console.log(DOMselectors.firstName.value);    
+    //DOMselectors.h2s.forEach(
+    //(el) => (el.textContent = DOMselectors.firstName.value)
+    //)
+    //})
+
+    
