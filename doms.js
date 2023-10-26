b4938a48f0ac3292e29c7c07bdf6c6e5291f5a3e
JoshRov2 <joshr16@nycstudents.net>
2023-10-26 09:24:37 -0400

    update

--- doms.js
+++ doms.js
@@ -39,32 +39,16 @@ function createHtmlCard(manufacturer, model, imageUrl) {
     const manufacturerInfo = `<p>Manufacturer: ${manufacturer}</p>`;
     const modelInfo = `<p>Model: ${model}</p>`;
 
-    htmlCardContent.insertAdjacentHTML('beforeend', manufacturerInfo);
+    htmlCardContent.insertAdjacentHTML('beforeend', `<button class=remove>Remove Car</button> ${manufacturerInfo}`);
     htmlCardContent.insertAdjacentHTML('beforeend', modelInfo);
-
-  /*   const manufacturerTitle = document.createElement('strong');
-    manufacturerTitle.textContent = 'Manufacturer:';
-    const manufacturerText = document.createElement('p');
-    manufacturerText.textContent = manufacturer; */
-
-    /* const modelTitle = document.createElement('strong');
-    modelTitle.textContent = 'Model:';
-    const modelText = document.createElement('p');
-    modelText.textContent = model;
-    htmlCardContent.innerHTML = `
-        <p><strong>Manufacturer:</strong> ${manufacturer}</p>
-        <p><strong>Model:</strong> ${model}</p>
-    `;
-    htmlCardContent.appendChild(manufacturerTitle);
-    htmlCardContent.appendChild(manufacturerText);
-    htmlCardContent.appendChild(modelTitle);
-    htmlCardContent.appendChild(modelText); */
-
+    
 
     htmlCard.appendChild(image);
     htmlCard.appendChild(htmlCardContent);
 
     DOMSelectors.htmlCards.appendChild(htmlCard);
+
+    
 }
 
 function clearInputFields() {
